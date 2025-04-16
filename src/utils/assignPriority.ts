218b8833ed3d81b44894d6315a6caf93ab0da919
Jakub Zieliński <jzielinski47dev@gmail.com>
2025-04-16 22:05:49 +0200

    updated emergency detection system

--- src/utils/assignPriority.ts
+++ src/utils/assignPriority.ts
@@ -3,19 +3,22 @@ import { Vehicle } from "../types/interfaces/vehicle.interface";
 
 export const getPriorityVehicle = (): Vehicle | null => {
     let longestWaitTimeIndex = 0;
-    let longestWaitTimeVeh: Vehicle | null = null;
+    let longestWaitTimeVehicle: Vehicle | null = null;
 
     for (const dir of directionNames) {
         const state = environment[dir];
 
         if (state.queue.length > 0) {
-            if (state.queue[0].isEmergencyVehicle) return state.queue[0];
-            if (state.queue[0].waitTime > longestWaitTimeIndex) {
-                longestWaitTimeIndex = state.queue[0].waitTime;
-                longestWaitTimeVeh = state.queue[0];
+            const emergency = state.queue.find((vehicle) => vehicle.isEmergencyVehicle);
+            if (emergency) return emergency;
+
+            const [frontVehicle] = state.queue;
+            if (frontVehicle.waitTime > longestWaitTimeIndex) {
+                longestWaitTimeIndex = frontVehicle.waitTime;
+                longestWaitTimeVehicle = frontVehicle;
             }
         }
     }
 
-    return longestWaitTimeVeh;
+    return longestWaitTimeVehicle;
 };
