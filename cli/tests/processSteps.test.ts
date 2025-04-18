f256b45f18b7e93ea7f7e90fbb00221c14e69593
Jakub Zieliński <jzielinski47dev@gmail.com>
2025-04-18 11:57:41 +0200

    new unit tests scenarios

--- cli/tests/processSteps.test.ts
+++ cli/tests/processSteps.test.ts
@@ -10,7 +10,7 @@ describe("processSteps", () => {
         hardEnvironmentReset();
     });
 
-    test("standard-input", () => {
+    test("should correctly process a standard traffic scenario", () => {
         const scenarioNumber: number = 1;
         const sampleInput: string = `./tests/mockedInputs/scenario${scenarioNumber}.json`;
         const sampleOutput: string = `./tests/mockedOutputs/scenario${scenarioNumber}.json`;
@@ -23,7 +23,7 @@ describe("processSteps", () => {
         expect(output).toMatchObject(expectedOut);
     });
 
-    test("multiple-vehicles-on-the-same-direction", () => {
+    test("should handle multiple vehicles approaching from the same direction", () => {
         const scenarioNumber: number = 2;
         const sampleInput: string = `./tests/mockedInputs/scenario${scenarioNumber}.json`;
         const sampleOutput: string = `./tests/mockedOutputs/scenario${scenarioNumber}.json`;
@@ -36,7 +36,7 @@ describe("processSteps", () => {
         expect(output).toMatchObject(expectedOut);
     });
 
-    test("multiple-vehicles-from-each-direction", () => {
+    test("should handle multiple vehicles approaching from all directions", () => {
         const scenarioNumber: number = 3;
         const sampleInput: string = `./tests/mockedInputs/scenario${scenarioNumber}.json`;
         const sampleOutput: string = `./tests/mockedOutputs/scenario${scenarioNumber}.json`;
@@ -49,7 +49,7 @@ describe("processSteps", () => {
         expect(output).toMatchObject(expectedOut);
     });
 
-    test("emergency-vehicle", () => {
+    test("should correctly prioritize a single emergency vehicle", () => {
         const scenarioNumber: number = 4;
         const sampleInput: string = `./tests/mockedInputs/scenario${scenarioNumber}.json`;
         const sampleOutput: string = `./tests/mockedOutputs/scenario${scenarioNumber}.json`;
@@ -62,8 +62,47 @@ describe("processSteps", () => {
         expect(output).toMatchObject(expectedOut);
     });
 
-    test("emergency-vehicle", () => {
-        const scenarioNumber: number = 4;
+    test("should move emergency vehicle to front if not first in queue", () => {
+        const scenarioNumber: number = 5;
+        const sampleInput: string = `./tests/mockedInputs/scenario${scenarioNumber}.json`;
+        const sampleOutput: string = `./tests/mockedOutputs/scenario${scenarioNumber}.json`;
+
+        const input = JSON.parse(fs.readFileSync(sampleInput, "utf-8"));
+        const output: Output = processSteps(input.commands);
+
+        const expectedOut = JSON.parse(fs.readFileSync(sampleOutput, "utf-8"));
+        console.log(output, expectedOut);
+        expect(output).toMatchObject(expectedOut);
+    });
+
+    test("should prioritize multiple emergency vehicles by forming a corridor", () => {
+        const scenarioNumber: number = 6;
+        const sampleInput: string = `./tests/mockedInputs/scenario${scenarioNumber}.json`;
+        const sampleOutput: string = `./tests/mockedOutputs/scenario${scenarioNumber}.json`;
+
+        const input = JSON.parse(fs.readFileSync(sampleInput, "utf-8"));
+        const output: Output = processSteps(input.commands);
+
+        const expectedOut = JSON.parse(fs.readFileSync(sampleOutput, "utf-8"));
+        console.log(output, expectedOut);
+        expect(output).toMatchObject(expectedOut);
+    });
+
+    test("should handle priority under heavy traffic of 28 vehicles at once", () => {
+        const scenarioNumber: number = 7;
+        const sampleInput: string = `./tests/mockedInputs/scenario${scenarioNumber}.json`;
+        const sampleOutput: string = `./tests/mockedOutputs/scenario${scenarioNumber}.json`;
+
+        const input = JSON.parse(fs.readFileSync(sampleInput, "utf-8"));
+        const output: Output = processSteps(input.commands);
+
+        const expectedOut = JSON.parse(fs.readFileSync(sampleOutput, "utf-8"));
+        console.log(output, expectedOut);
+        expect(output).toMatchObject(expectedOut);
+    });
+
+    test("should handle priority under heavy traffic of 28 vehicles in different time", () => {
+        const scenarioNumber: number = 8;
         const sampleInput: string = `./tests/mockedInputs/scenario${scenarioNumber}.json`;
         const sampleOutput: string = `./tests/mockedOutputs/scenario${scenarioNumber}.json`;
 
