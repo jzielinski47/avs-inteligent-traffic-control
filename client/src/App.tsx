367754156645ee3cb2648d09d503f3de0c31a0e4
Jakub Zieliński <jzielinski47dev@gmail.com>
2025-04-18 21:24:15 +0200

    initial_client_setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+    return (
+        <div className="app flex p-4 justify-start items-start w-full h-full">
+            <div className="flex flex-col items-start gap-1">
+                <h2 className="font-bold text-2xl">AVS - Intelligent traffic control system</h2>
+                <p className="text-lg text-white/60">by Jakub Zieliński</p>
+            </div>
+        </div>
+    );
+};
+
+export default App;
